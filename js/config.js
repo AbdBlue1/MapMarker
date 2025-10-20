f93eac4af9c72abf20d9d4763aa85f43eb564e0f
AbdBlue1 <AbdBlue@icloud.com>
2025-10-20 03:01:26 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,7 @@
+// Mapbox Configuration Example
+// This is a template file that CAN be safely uploaded to GitHub
+// Copy this file to config.js and add your actual token
+
 const CONFIG = {
-    MAPBOX_TOKEN: 'YOUR_MAPBOX_ACCESS_TOKEN'
+    MAPBOX_TOKEN: 'pk.eyJ1IjoiYWJkYWJkdGEiLCJhIjoiY21neWNudHhoMHdhcDJrc2QzZ25haG5kOCJ9.VMIIH5r1sKpvjl_dUbicww'
 };
